cd231f52b983446fe68e8fc2c904930ff61c590c
Fantix King <fantix@uchicago.edu>
2019-12-13 11:32:00 -0800

    print environ

--- entrypoint.py
+++ entrypoint.py
@@ -11,3 +11,17 @@ with open(os.environ["GITHUB_EVENT_PATH"]) as f:
 repo = Repo()
 print("Repo: ", repo)
 print("Payload: ", payload)
+print("Environ: ", os.environ)
+
+# payload = {
+#     "pages": [
+#         {
+#             "action": "created",
+#             "html_url": "https://github.com/fantix/release-demo/wiki/Home",
+#             "page_name": "Home",
+#             "sha": "16edb380041ef742db82b6bf1beae4defeea083a",
+#             "summary": None,
+#             "title": "Home",
+#         }
+#     ],
+# }
